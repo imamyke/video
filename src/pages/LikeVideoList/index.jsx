d927e5167bea533a014e24f3db622b22bbe1b1d1
DavidChung0305 <qwert9789789469@gmail.com>
2024-08-02 20:01:04 +0800

    feat:o

--- src/pages/LikeVideoList/index.jsx
+++ src/pages/LikeVideoList/index.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import beigow from "@/assets/images/beigow.jpeg"
 import { useState, useEffect } from "react"
 
 const LikeVideoList= () =>{
@@ -15,7 +16,12 @@ const LikeVideoList= () =>{
   return(
     <div className="flex mt-4 ">
       <div className="w-[350px] h-[622px] bg-slate-400 rounded-2xl">
-
+        <div className="mx-auto w-[280px]">
+          <img src={beigow} className="w-full h-[180px] mt-5 rounded-lg"/>
+          <h1 className="text-[30px] font-bold  mt-5">喜歡的影片</h1>
+          <p className="font-bold  mt-3">惠勝</p>
+          <p className="text-[13px]  mt-2 text-slate-500">722 部影片    觀看次數：2次</p>
+        </div>
       </div>
       <div className="ml-2">
         <i className="fa-solid fa-arrows-up-down w-[450px] h-[60px] text-[30px] pl-3 pt-3"></i>
