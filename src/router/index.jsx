9dc023475616b2b5a317953462ab2c6c585ff480
Ssuyu Ke <ssuyuke@asy.tw>
2024-08-12 20:37:48 +0800

    deploy

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Home, Login, NotFound, Channel, Video, Short, UserChannel,  LikeVideoList, PlaylistLater } from "@/pages";
 import Layout from "@/components/Layout";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Layout/>,
